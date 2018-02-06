4f4491efd8000bf2eb68ed93f56205227584d43f
胡钢玺 <hugangxi@tttdeMacBook-Pro.local>
2018-02-06 17:00:33 +0800

    测试账号

--- laravel/app/Http/Controllers/WxController.php
+++ laravel/app/Http/Controllers/WxController.php
@@ -8,30 +8,51 @@
 
 namespace App\Http\Controllers;
 
-
+use EasyWeChat\Factory;
+use EasyWeChat\Payment\Application;
 class WxController extends Controller
 {
     public function index()
     {
-        if(isset($_GET['echostr'])) {
-            $signatrue   = $_GET['signatrue'];
-            $token         = 'weixin';
-            $timestamp = $_GET['timestamp'];
-            $nonce         = $_GET['nonce'];
-            $arr = [$token, $timestamp, $nonce];
-            sort($arr, SORT_STRING);
 
-            $str = implode('', $arr);
-            $str = sha1($str);
+        $options = [
+            'debug'  => true,
+            'app_id'  => 'wx25aa36a54cfd3f2a',
+            'secret'   => 'ead7750606259b3984876560715172f9',
+            'token'    => 'weixin',
+            'log'       => [
+                'level'   =>  'debug',
+                'file'     =>__DIR__.'/wechat.log',
+            ]
+        ];
+//
 
-            if ($str === $signatrue) {
-                return $_GET['echostr'];
-                exit;
-            }
+        $app  = new Application($options);
+        $response = $app->server->serve();
+        return $response;
 
-        }
-        else
-        {
+
+
+//        new Factory()
+//        if(isset($_GET['echostr'])) {
+//            $signatrue   = $_GET['signatrue'];
+//            $token         = 'weixin';
+//            $timestamp = $_GET['timestamp'];
+//            $nonce         = $_GET['nonce'];
+//            $arr = [$token, $timestamp, $nonce];
+//            sort($arr, SORT_STRING);
+//
+//            $str = implode('', $arr);
+//            $str = sha1($str);
+//
+//            if ($str === $signatrue) {
+//                return $_GET['echostr'];
+//                exit;
+//            }
+
+//        }
+//        else
+//        {
 
             //        $xml = "<xml>
             //                   <ToUserName><![CDATA[toUser]]></ToUserName>
@@ -41,27 +62,27 @@ class WxController extends Controller
             //                   <Content><![CDATA[this is a test]]></Content>
             //                   <MsgId>1234567890123456</MsgId>
             //                 </xml>";
-                        $xml = $HTTP_RAW_POST_DATA;
-                        $postObj = simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA);
+//                        $xml = $HTTP_RAW_POST_DATA;
+//                        $postObj = simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA);
+//
+//
+//
+//                    $toUsername = $postObj->FromUserName; //用户微信号
+//                    $fromUsername = $postObj->ToUserName; //开发者微信号
+//                    $msgType = $postObj->MsgType; //回复类型
+//                    $content = $postObj->Content;   //获取用户发的信息
+//                    $time = time(); //时间戳
+//
+//                    $tpl = "<xml>  <ToUserName><![CDATA[".$toUsername."]]></ToUserName>
+//                                   <FromUserName><![CDATA[".$fromUsername."]]></FromUserName>
+//                                   <CreateTime>".time()."</CreateTime>
+//                                   <MsgType><![CDATA[text]]></MsgType>
+//                                   <Content><![CDATA[".$content."]]></Content>
+//                                   </xml>";
+//                    echo $tpl;
 
 
 
-                    $toUsername = $postObj->FromUserName; //用户微信号
-                    $fromUsername = $postObj->ToUserName; //开发者微信号
-                    $msgType = $postObj->MsgType; //回复类型
-                    $content = $postObj->Content;   //获取用户发的信息
-                    $time = time(); //时间戳
-
-                    $tpl = "<xml>  <ToUserName><![CDATA[".$toUsername."]]></ToUserName>
-                                   <FromUserName><![CDATA[".$fromUsername."]]></FromUserName>
-                                   <CreateTime>".time()."</CreateTime>
-                                   <MsgType><![CDATA[text]]></MsgType>
-                                   <Content><![CDATA[".$content."]]></Content>
-                                   </xml>";
-                    echo $tpl;
-
-
-
-        }
+//        }
     }
 }
