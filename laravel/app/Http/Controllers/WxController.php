00822ecf0178dc80e17ae65b3697896d91fa9d61
胡钢玺 <hugangxi@tttdeMacBook-Pro.local>
2018-02-06 22:38:59 +0800

    在测试里面提交

--- laravel/app/Http/Controllers/WxController.php
+++ laravel/app/Http/Controllers/WxController.php
@@ -8,73 +8,29 @@
 
 namespace App\Http\Controllers;
 
-use EasyWeChat\Factory;
-use EasyWeChat\Payment\Application;
 class WxController extends Controller
 {
     public function index()
     {
 
-//        $options = [
-//            'debug'  => true,
-//            'app_id'  => 'wx25aa36a54cfd3f2a',
-//            'secret'   => 'ead7750606259b3984876560715172f9',
-//            'token'    => 'weixin',
-//            'log'       => [
-//                'level'   =>  'debug',
-//                'file'     =>__DIR__.'/wechat.log',
-//            ]
-//        ];
+
+            $signatrue   = $_GET['signatrue'];
+            $token         = 'zhangyu';
+            $timestamp = $_GET['timestamp'];
+            $nonce         = $_GET['nonce'];
+            $arr = [$token, $timestamp, $nonce];
+            sort($arr, SORT_STRING);
+
+            $str = implode('', $arr);
+            $str = sha1($str);
+
+            if ($str === $signatrue) {
+                return $_GET['echostr'];
+                exit;
+            }
+
 //
-//        $app  = new Application($options);
-//        $response = $app->server->serve();
-//        return $response;
-        $signatrue   = $_GET['signatrue'];
-        $token         = 'weixin';
-        $timestamp = $_GET['timestamp'];
-        $nonce         = $_GET['nonce'];
-        $arr = [$token, $timestamp, $nonce];
-        sort($arr, SORT_STRING);
-
-        $str = implode('', $arr);
-        $str = sha1($str);
-
-        if ($str === $signatrue) {
-            return $_GET['echostr'];
-            exit;
-        }
-
-
-//        new Factory()
-//        if(isset($_GET['echostr'])) {
-//            $signatrue   = $_GET['signatrue'];
-//            $token         = 'weixin';
-//            $timestamp = $_GET['timestamp'];
-//            $nonce         = $_GET['nonce'];
-//            $arr = [$token, $timestamp, $nonce];
-//            sort($arr, SORT_STRING);
-//
-//            $str = implode('', $arr);
-//            $str = sha1($str);
-//
-//            if ($str === $signatrue) {
-//                return $_GET['echostr'];
-//                exit;
-//            }
-
-//        }
-//        else
-//        {
-
-            //        $xml = "<xml>
-            //                   <ToUserName><![CDATA[toUser]]></ToUserName>
-            //                   <FromUserName><![CDATA[fromUser]]></FromUserName>
-            //                   <CreateTime>1348831860</CreateTime>
-            //                   <MsgType><![CDATA[text]]></MsgType>
-            //                   <Content><![CDATA[this is a test]]></Content>
-            //                   <MsgId>1234567890123456</MsgId>
-            //                 </xml>";
-//                        $xml = $HTTP_RAW_POST_DATA;
+//                        $xml =  $GLOBALS['HTTP_RAW_POST_DATA'];
 //                        $postObj = simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA);
 //
 //
@@ -85,12 +41,13 @@ class WxController extends Controller
 //                    $content = $postObj->Content;   //获取用户发的信息
 //                    $time = time(); //时间戳
 //
-//                    $tpl = "<xml>  <ToUserName><![CDATA[".$toUsername."]]></ToUserName>
-//                                   <FromUserName><![CDATA[".$fromUsername."]]></FromUserName>
-//                                   <CreateTime>".time()."</CreateTime>
+//                    $tpl = "<xml>  <ToUserName><![CDATA[%s]]></ToUserName>
+//                                   <FromUserName><![CDATA[$fromUsername]]></FromUserName>
+//                                   <CreateTime>time()</CreateTime>
 //                                   <MsgType><![CDATA[text]]></MsgType>
-//                                   <Content><![CDATA[".$content."]]></Content>
+//                                   <Content><![CDATA[$content]]></Content>
 //                                   </xml>";
+//                    $resultSte = sprintf($tpl,$toUsername,$fromUsername,$time,$MsgType,$content);
 //                    echo $tpl;
 
 
