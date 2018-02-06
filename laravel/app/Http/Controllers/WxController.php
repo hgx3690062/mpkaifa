810fc0b0f0a6a0b997d69b36fcd1a37025be0461
胡钢玺 <hugangxi@tttdeMacBook-Pro.local>
2018-02-06 15:59:45 +0800

    接收xml数据包和返回信息

--- laravel/app/Http/Controllers/WxController.php
+++ laravel/app/Http/Controllers/WxController.php
@@ -13,43 +13,56 @@ class WxController extends Controller
 {
     public function index()
     {
+        $signatrue   = $_GET['signatrue'];
+        $token         = 'weixin';
+        $timestamp = $_GET['timestamp'];
+        $nonce         = $_GET['nonce'];
 
-//        $postStr = isset($GLOBALS['HTTP_RAW_POST_DATA']) ? $GLOBALS['HTTP_RAW_POST_DATA'] : file_get_contents("php://input");
-        $xml = "<xml> 
-                   <ToUserName><![CDATA[toUser]]></ToUserName>  
-                   <FromUserName><![CDATA[fromUser]]></FromUserName>  
-                   <CreateTime>1348831860</CreateTime>  
-                   <MsgType><![CDATA[text]]></MsgType>  
-                   <Content><![CDATA[this is a test]]></Content>  
-                   <MsgId>1234567890123456</MsgId>  
-                 </xml>";
-        $postObj = simplexml_load_string($xml,'SimpleXMLElement',LIBXML_NOCDATA);
+        if($_GET['echostr']) {
 
-         var_dump($postObj);
-         echo $postObj->ToUserName;
-         echo '<br>';
-         echo $postObj->Content;
+            $arr = [$token, $timestamp, $nonce];
+            sort($arr, SORT_STRING);
+
+            $str = implode('', $arr);
+            $str = sha1($str);
+
+            if ($str === $signatrue) {
+                return $_GET['echostr'];
+                exit;
+            }
+
+        }
+        else {
+
+            //        $xml = "<xml>
+            //                   <ToUserName><![CDATA[toUser]]></ToUserName>
+            //                   <FromUserName><![CDATA[fromUser]]></FromUserName>
+            //                   <CreateTime>1348831860</CreateTime>
+            //                   <MsgType><![CDATA[text]]></MsgType>
+            //                   <Content><![CDATA[this is a test]]></Content>
+            //                   <MsgId>1234567890123456</MsgId>
+            //                 </xml>";
+                        $xml = $GLOBALS['HTTP_RAW_POST_DATA'];
+                        $postObj = simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA);
 
 
-//        $toUsername = $postObj->FromUserName; //用户微信号
-//        $fromUsername = $postObj->ToUserName; //开发者微信号
-//        $msgType = $postObj->MsgType; //回复类型
-//        $content = $postObj->Content;   //获取用户发的信息
-//        $time = time(); //时间戳
-//
-//        $tpl = "<xml>  <ToUserName>< ![CDATA[%s] ]></ToUserName>
-//                       <FromUserName>< ![CDATA[%s] ]></FromUserName> 
-//                       <CreateTime>%s</CreateTime> 
-//                       <MsgType>< ![CDATA[%s] ]></MsgType>
-//                       <Content>< ![CDATA[%s] ]></Content>
-//                       </xml>";
-//
-//        if($msgType == 'text')
-//        {
-//            $msgtype = 'text';
-//            $contents = '张誉';
-//            $sprintf = sprintf($tpl,$toUsername,$fromUsername,$time,$msgtype,$contents);
-//            echo $sprintf;
-//        }
+
+                    $toUsername = $postObj->FromUserName; //用户微信号
+                    $fromUsername = $postObj->ToUserName; //开发者微信号
+                    $msgType = $postObj->MsgType; //回复类型
+                    $content = $postObj->Content;   //获取用户发的信息
+                    $time = time(); //时间戳
+
+                    $tpl = "<xml>  <ToUserName><![CDATA[".$toUsername."]]></ToUserName>
+                                   <FromUserName><![CDATA[".$fromUsername."]]></FromUserName>
+                                   <CreateTime>".time()."</CreateTime>
+                                   <MsgType><![CDATA[text]]></MsgType>
+                                   <Content><![CDATA[".$content."]]></Content>
+                                   </xml>";
+                    echo $tpl;
+
+
+
+        }
     }
 }
