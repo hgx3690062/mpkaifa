dde40ae96dee34f8e8c1b78cc1ea9e963c935b82
胡钢玺 <hugangxi@tttdeMacBook-Pro.local>
2018-02-07 10:56:17 +0800

    从新开发

--- laravel/app/Http/Controllers/WxController.php
+++ laravel/app/Http/Controllers/WxController.php
@@ -8,11 +8,20 @@
 
 namespace App\Http\Controllers;
 use EasyWeChat\Factory;
+use Log;
 
 class WxController extends Controller
 {
     public function index()
     {
+//        Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
+//
+//        $app = app('wechat.official_account');
+//        $app->server->push(function($message){
+//            return "欢迎关注 overtrue！";
+//        });
+//
+//        return $app->server->serve();
 
         $config = [
             'app_id' => 'wx8d75fb66b9f2a882',
