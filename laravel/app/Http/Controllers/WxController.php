5fba77b5a45a9a2d3596161000a49adb651c2fec
胡钢玺 <hugangxi@tttdeMacBook-Pro.local>
2018-02-08 11:28:09 +0800

    改为测试

--- laravel/app/Http/Controllers/WxController.php
+++ laravel/app/Http/Controllers/WxController.php
@@ -34,7 +34,6 @@ class WxController extends Controller
     }
      public function index(){
 
-
          $this->app->server->push(function ($message) {
 
              if($message['MsgType'] == 'event'){
@@ -42,16 +41,15 @@ class WxController extends Controller
              }
              if($message['MsgType'] == 'text')
              {
-//                 $items = [
-//                     new NewsItem([
-//                         'title'       => '张誉',
-//                         'description' => '时间如在昨日',
-//                         'url'         => 'www.baidu.com',
-//                         'image'       => 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1518594074&di=8b54035ad2274c1f5a84c183dc24b895&imgtype=jpg&er=1&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F01711b59426ca1a8012193a31e5398.gif',
-//                     ]),
-//                ];
-//                return new News($items);
-                return  new Text($this->app->user->select('olk48wcsvYThfHhdwOJGJOjlxfLg'));
+                 $items = [
+                     new NewsItem([
+                         'title'       => '张誉',
+                         'description' => '时间如在昨日',
+                         'url'         => 'www.baidu.com',
+                         'image'       => 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1518594074&di=8b54035ad2274c1f5a84c183dc24b895&imgtype=jpg&er=1&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F01711b59426ca1a8012193a31e5398.gif',
+                     ]),
+                ];
+                return new News($items);
 
              }
 
@@ -66,6 +64,12 @@ class WxController extends Controller
         $token = file_get_contents('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx25aa36a54cfd3f2a&secret=ead7750606259b3984876560715172f9');
         $token = json_decode($token,true);
         return $token['access_token'];
+    }
+
+    //自定义菜单
+    public function text()
+    {
+        return view('text');
     }
 
 
