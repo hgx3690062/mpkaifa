07da217581f2ea33d1964dda9f89c4a5c64004b8
胡钢玺 <hugangxi@tttdeMacBook-Pro.local>
2018-02-08 17:36:26 +0800

    网页授权

--- laravel/app/Http/Controllers/WxController.php
+++ laravel/app/Http/Controllers/WxController.php
@@ -72,6 +72,7 @@ class WxController extends Controller
 
          $oauth = $this->app->oauth;
          if (!session()->has('wechat_user')) {
+             dd(2);
 
              session(['target_url'=>'user/text']);
 
@@ -79,7 +80,7 @@ class WxController extends Controller
              // 这里不一定是return，如果你的框架action不是返回内容的话你就得使用
              // $oauth->redirect()->send();
          }
-         dd(213);
+         dd(1);
 
 // 已经登录过
          $user = session('wechat_user');
