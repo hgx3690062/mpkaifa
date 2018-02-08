5dc813df50e927acd95c2e50122c3175b5a4a4f1
胡钢玺 <hugangxi@tttdeMacBook-Pro.local>
2018-02-08 17:21:04 +0800

    网页授权

--- laravel/app/Http/Controllers/WxController.php
+++ laravel/app/Http/Controllers/WxController.php
@@ -63,7 +63,6 @@ class WxController extends Controller
        session(['wechat_user'=>$user->toArray()]);
 
         $targetUrl = session()->has('target_url') ?  session('target_url'):'/' ;
-        dd($user);
         header('location:'. $targetUrl); // 跳转到 user/profile
     }
 
