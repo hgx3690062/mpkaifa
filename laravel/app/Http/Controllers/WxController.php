9becb131ca8759bb1606fb9309f085c5bf52b1d6
胡钢玺 <hugangxi@tttdeMacBook-Pro.local>
2018-02-09 10:48:37 +0800

    新的网页授权方式

--- laravel/app/Http/Controllers/WxController.php
+++ laravel/app/Http/Controllers/WxController.php
@@ -52,24 +52,19 @@ class WxController extends Controller
     }
 
      public function index(){
+         $oauth = $this->app->oauth;
+         if (!session()->has('wechat_user')) {
 
-         $targetUrl = 'user/text';
-         $response = $this->app->oauth->scopes(['snsapi_userinfo'])
-             ->redirect(url($targetUrl));
-         return $response;
-//         $oauth = $this->app->oauth;
-//         if (!session()->has('wechat_user')) {
-//
-//
-//             session(['target_url'=>'user/text']);
-//
-//             return $oauth->redirect();
-//             // 这里不一定是return，如果你的框架action不是返回内容的话你就得使用
-//             // $oauth->redirect()->send();
-//         }
-//
-//          // 已经登录过
-//         $user = session('wechat_user');
+
+             session(['target_url'=>'user/text']);
+
+             return $oauth->redirect();
+             // 这里不一定是return，如果你的框架action不是返回内容的话你就得使用
+             // $oauth->redirect()->send();
+         }
+
+          // 已经登录过
+         $user = session('wechat_user');
 
     }
     public function text()
